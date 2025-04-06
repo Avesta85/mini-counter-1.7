ae3335cd49ea87b0519851cde69ee331b48e5e80
Avesta <avesta.alamoutifard85@gmail.com>
2025-04-06 17:06:58 +0330

    fix map in player classes and moved in ct and  Terrorist classes

--- main classes/player.h
+++ main classes/player.h
@@ -1,23 +1,53 @@
 #pragma once
 #include <string>
 #include "myrandom.h"
+#include "linked_list_D&R.h"
+#include <unordered_map>
+#include "Gun.h"
 
 using std::string;
 
 
 class player {
 
+	player();
+	player(string, int, int, int, bool);
+	~player();
+
+	//getter
+
+	string get_name();
+	string get_id();
+	string get_holdGun_id();
+	string get_holdGun_name();
+	int get_health();
+	int get_armor();
+	bool is_Alive();
+	bool is_ABot();
+	int get_monye();
+
+	//setter 
+	void set_name(string);
+	void set_health(int);
+	void set_armor(int);
+	void set_Alive(bool);
+	void set_monye(int);
 
 
+	//control
 
+	void next_gun();
+	void prev_gun();
 
-
+	void pickGun(string);
+	void dropGun(string);
 
 private:
+
 	string name;
 	string Guid;
 	
-	int holdGun_Id;
+	string holdGun_Id;
 	string holdGun_Name;
 
 	int health;
@@ -27,5 +57,8 @@ private:
 	bool isBot;
 
 	int monye;
+
+	DRL_list_Guid GunId_List;
+
 	
 };
