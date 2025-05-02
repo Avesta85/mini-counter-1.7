c10804bb0c4057eaacaabeaa744d34d80156ae1f
Avesta <avesta.alamoutifard85@gmail.com>
2025-05-02 03:59:12 +0330

    add main runner

---
+++ mini counter 1.7.cpp
@@ -0,0 +1,15 @@
+
+#include "main classes/menu/menu.h"
+//#include "main classes/GunModel.h"
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	menu m;  
+
+	m.Start_Screen_Control(argc,argv);   
+
+
+}
